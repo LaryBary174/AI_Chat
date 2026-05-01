1e68e2d297602e3a2c5323a8066d1ccc009672f5
Denis Nagovitsyn <lotuc74@gmail.com>
2026-05-01 12:12:12 +0500

    используется реальный streaming, а не имитация

--- giga.py
+++ giga.py
@@ -1,25 +1,14 @@
-import asyncio
 from typing import AsyncGenerator
 from gigachat import GigaChat
 
 from settings import settings
 
 
-async def get_response_from_ai(prompt: str) -> str:
+async def get_giga_streaming(prompt: str) -> AsyncGenerator[str, None]:
+    """Потоковая передача ответа от GigaChat (Streaming)"""
     async with GigaChat(
         credentials=settings.AI_API_KEY,
         verify_ssl_certs=False,
     ) as giga:
-
-        resp = giga.chat(prompt)
-
-        return resp.choices[0].message.content
-
-
-async def get_giga_streaming(prompt: str) -> AsyncGenerator[str, None]:
-    """Потоковая передача ответа от GigaChat (Streaming)"""
-    full_text = await get_response_from_ai(prompt)
-
-    for text in full_text.split():
-        await asyncio.sleep(0.1)
-        yield text + ' '
+        async for text in giga.astream(prompt):
+            yield text.choices[0].delta.content
